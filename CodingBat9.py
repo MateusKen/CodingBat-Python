45cd41f507474d4da055297a691c6300ff95ffc0
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-10-06 23:21:52 -0300

    Create CodingBat9.py

---
+++ CodingBat9.py
@@ -0,0 +1,12 @@
+'''
+exercícios: warmup-1 (not_string)
+https://codingbat.com/prob/p189441
+'''
+
+def not_string(str):
+  Str = str.split(' ')
+  if Str[0] != 'not':
+    str = 'not ' +str
+    return str
+  else:
+    return str
