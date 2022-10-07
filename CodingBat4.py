bdccd9fdd287772ada46036670fc205ffe1272a3
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-10-06 22:56:59 -0300

    Create CodingBat4.py

---
+++ CodingBat4.py
@@ -0,0 +1,12 @@
+'''
+exercício: warmup-1 (sum_double)
+https://codingbat.com/prob/p141905
+'''
+
+def sum_double(a, b):
+  if a == b:
+    dsum = 2*(a+b)
+    return dsum
+  else:
+    sum = a+b
+    return sum
