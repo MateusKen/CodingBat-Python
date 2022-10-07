8b8d591e54485bcc9090af7f8e77ebae80b810c7
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-10-06 22:47:32 -0300

    Create CodingBat2.py

---
+++ CodingBat2.py
@@ -0,0 +1,12 @@
+'''
+exercício: warmup-1 (sleep_in)
+https://codingbat.com/prob/p173401
+'''
+
+def sleep_in(weekday, vacation):
+  if vacation == True:
+    return True
+  elif weekday == False:
+    return True
+  else:
+    return False
